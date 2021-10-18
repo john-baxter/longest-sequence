3263b11f5e160cd1c09558cc3d8d59538efd9044
john-baxter <john.g.baxter@googlemail.com>
2021-10-18 14:14:35 +0100

    Add more tests and refactor get_index_difference(). Function now working.

--- longest_sequence.py
+++ longest_sequence.py
@@ -1,3 +1,5 @@
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
 def find_longest_sequence(origin_string):
   disallowed_letters = "z"
   for character in disallowed_letters:
@@ -7,7 +9,49 @@ def find_longest_sequence(origin_string):
 
 
 def get_index_difference(origin_string):
-  if origin_string == "abz":
-    return [1, 22]
-  else:
-    return [1]
+  index_difference_list = []
+  for i in range(len(origin_string) - 1):
+    
+    # # print letter at position i in origin_string
+    # print(origin_string[i])
+
+    # # print alphabet index of THIS
+    # print(ALPHABET.index(origin_string[i]))
+
+    # # print alphabet index of NEXT letter in origin_string
+    # print(ALPHABET.index(origin_string[i+1]))
+
+    index_difference_list.append(
+      ALPHABET.index(origin_string[i+1]) - 
+      ALPHABET.index(origin_string[i])
+    )
+
+  return index_difference_list
+
+  """
+    FOR LOOP
+      get origin_string[i] 
+      find that letter in ALPHABET
+      get the index of that letter
+
+      get origin_string[i+1]
+      find that letter in ALPHABET
+      get the index of that letter
+
+      subtract one index from the other (make sure of the correct order)
+      add this integer into a new list
+    END FOR
+
+    return the new list
+  """
+  
+  # if origin_string == "abz":
+  #   return [1, 22]
+  # elif origin_string == "zab":
+  #   return [23, 1]
+  # else:
+  #   return [1]
+
+
+if __name__ == '__main__':
+  print(get_index_difference("zab"))
--- longest_sequence_test.py
+++ longest_sequence_test.py
@@ -50,17 +50,28 @@ class GetIndexDifferenceTest(unittest.TestCase):
     actual_result = longest_sequence.get_index_difference(test_origin_string)
     self.assertEqual(actual_result, expected_result)
 
-  def test_index_difference_returns_1_22_if_given_abz(self):
+  def test_index_difference_returns_1_24_if_given_abz(self):
     test_origin_string = "abz"
-    expected_result = [1, 22]
+    expected_result = [1, 24]
     actual_result = longest_sequence.get_index_difference(test_origin_string)
     self.assertEqual(actual_result, expected_result)
 
-  def test_index_difference_returns_23_1_if_given_zab(self):
+  def test_index_difference_returns_25_1_if_given_zab(self):
     test_origin_string = "zab"
-    expected_result = [23, 1]
+    expected_result = [-25, 1]
     actual_result = longest_sequence.get_index_difference(test_origin_string)
     self.assertEqual(actual_result, expected_result)
+
+  def test_check_longer_strings(self):
+    test_origin_string_one = "zabczzabcdz"
+    test_origin_string_two = "yabcyyabcdy"
+    expected_result_one = [-25, 1, 1, 23, 0, -25, 1, 1, 1, 22]
+    expected_result_two = [-24, 1, 1, 22, 0, -24, 1, 1, 1, 21]
+    actual_result_one = longest_sequence.get_index_difference(test_origin_string_one)
+    actual_result_two = longest_sequence.get_index_difference(test_origin_string_two)
+    self.assertEqual(actual_result_two, expected_result_two)
+    self.assertEqual(actual_result_one, expected_result_one)
+    
 
 
 
