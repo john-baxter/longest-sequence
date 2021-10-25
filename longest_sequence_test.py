4e5c232b105b386d32c0676700fa8a4d43f0169c
john-baxter <john.g.baxter@googlemail.com>
2021-10-25 11:46:32 +0100

    Add one more test to get_index_of_ones() to verify length of big_string_of_ones is correctly included in return tuple.

--- longest_sequence_test.py
+++ longest_sequence_test.py
@@ -114,11 +114,11 @@ class GetIndexOfOnesSequenceTest(unittest.TestCase):
     actual_result = longest_sequence.get_index_of_ones(test_index_difference_list)
     self.assertEqual(actual_result[1], expected_result)
 
-  # def test_check_get_index_of_ones_returns_length_of_4_for_0_1_1_0_1_1_1(self):
-  #   test_index_difference_list = [0, 1, 1, 0, 1, 1, 1]
-  #   expected_result = 3
-  #   actual_result = longest_sequence.get_index_of_ones(test_index_difference_list)
-  #   self.assertEqual(actual_result[1], expected_result)
+  def test_check_get_index_of_ones_returns_length_of_4_for_0_1_1_0_1_1_1(self):
+    test_index_difference_list = [0, 1, 1, 0, 1, 1, 1]
+    expected_result = 3
+    actual_result = longest_sequence.get_index_of_ones(test_index_difference_list)
+    self.assertEqual(actual_result[1], expected_result)
 
 
   
